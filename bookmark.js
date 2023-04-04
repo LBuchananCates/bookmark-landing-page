b5af73cd8a9eaa4f52b6282947feb2eca7275a0d
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-04-04 13:31:38 -0400

    adds successful function tab 1 in features-section, but other 2 tabs don't work, and refresh page displays all 3 tabs at once

--- bookmark.js
+++ bookmark.js
@@ -1,41 +1,69 @@
+// MOBILE MENU FUNCTIONALITY
 const openMobileMenuButton = document.getElementById("hamburger-menu-icon");
 const closeMobileMenuButton = document.getElementById("icon-close");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-const simpleBookmarking = document.getElementById("simple-bookmarking");
-const speedySearching = document.getElementById("speedy-searching");
-const easySharing = document.getElementById("easy-sharing");
-
-// opens menu WORKS
 openMobileMenuButton.addEventListener("click", function () {
   mobileMenu.classList.add("open-menu");
   mobileMenu.classList.remove("close-menu");
 });
 
-// closes menu WORKS
 closeMobileMenuButton.addEventListener("click", function () {
   mobileMenu.classList.remove("open-menu");
   mobileMenu.classList.add("close-menu");
 });
 
-// features-section simple bookmarking button displays "bookmark in one click" paragraph
-simpleBookmarking.addEventListener("click", function () {
-  console.log(simpleBookmarking);
-  // speedySearching display: none;
-  // easySharing display: none;
-  // use classList.add/remove or textContent changes
+// FEATURES WITH BUTTONS (3 TABS)
+const simpleBookmarkingButton = document.getElementById("simple-bookmarking");
+const simpleBookmarkingContainer = document.getElementById(
+  "simple-bookmarking-container"
+);
+const speedySearchingButton = document.getElementById("speedy-searching");
+const speedySearchingContainer = document.getElementById(
+  "speedy-searching-container"
+);
+const easySharingButton = document.getElementById("easy-sharing");
+const easySharingContainer = document.getElementById("easy-sharing-container");
+
+// features-section simple bookmarking button displays "bookmark in one click" paragraph WORKS BUT OTHERS DON'T
+simpleBookmarkingButton.addEventListener("click", function () {
+  speedySearchingContainer.style.display = "none";
+  easySharingContainer.style.display = "none";
 });
 
 // features-section speedy searching button displays "intelligent search" paragraph
-speedySearching.addEventListener("click", function () {
-  console.log(speedySearching);
-  // simpleBookmarking display: none;
-  // easySharing display: none;
+speedySearchingButton.addEventListener("click", function () {
+  simpleBookmarkingContainer.style.display = "none";
+  easySharingContainer.style.display = "none";
 });
 
 // features-section easy sharing button displays "share your bookmarks" paragraph
-easySharing.addEventListener("click", function () {
-  console.log(easySharing);
-  // simpleBookmarking display: none;
-  // speedySearching display: none;
+easySharingButton.addEventListener("click", function () {
+  simpleBookmarkingContainer.style.display = "none";
+  speedySearchingContainer.style.display = "none";
 });
+
+// FAQ TOGGLE SECTION
+
+// EMAIL VERIFICATION
+// grab email and error message by id
+const email = document.getElementById("email");
+const emailErrorMessage = document.getElementById("error-message");
+
+// use regex for email
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+function validation() {
+  if (email.value.match(pattern)) {
+    email.classList.add("valid");
+    email.classList.remove("invalid");
+    // style valid and invalid classes in CSS
+    emailErrorMessage.style.visibility = "hidden";
+  } else {
+    email.classList.remove("valid");
+    email.classList.add("invalid");
+    emailErrorMessage.style.visibility = "visible";
+  }
+}
+// show error border if email entered incorrectly: style in CSS
+// remove error border and error messages when email format is correct
+// reference signup-form.js
