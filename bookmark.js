ca29a109c5790215116bdad570b3041ae664dee8
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-04-04 13:09:02 -0400

    comments for features-section displaying corresponding image and blurb when clicked

--- bookmark.js
+++ bookmark.js
@@ -2,11 +2,9 @@ const openMobileMenuButton = document.getElementById("hamburger-menu-icon");
 const closeMobileMenuButton = document.getElementById("icon-close");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-const simplyBookmarking = document.getElementById("simply-bookmarking");
+const simpleBookmarking = document.getElementById("simple-bookmarking");
 const speedySearching = document.getElementById("speedy-searching");
 const easySharing = document.getElementById("easy-sharing");
-
-// when click HAMB or X button, mobile-menu slides in and out
 
 // opens menu WORKS
 openMobileMenuButton.addEventListener("click", function () {
@@ -14,8 +12,30 @@ openMobileMenuButton.addEventListener("click", function () {
   mobileMenu.classList.remove("close-menu");
 });
 
-// closes menu
+// closes menu WORKS
 closeMobileMenuButton.addEventListener("click", function () {
   mobileMenu.classList.remove("open-menu");
   mobileMenu.classList.add("close-menu");
+});
+
+// features-section simple bookmarking button displays "bookmark in one click" paragraph
+simpleBookmarking.addEventListener("click", function () {
+  console.log(simpleBookmarking);
+  // speedySearching display: none;
+  // easySharing display: none;
+  // use classList.add/remove or textContent changes
+});
+
+// features-section speedy searching button displays "intelligent search" paragraph
+speedySearching.addEventListener("click", function () {
+  console.log(speedySearching);
+  // simpleBookmarking display: none;
+  // easySharing display: none;
+});
+
+// features-section easy sharing button displays "share your bookmarks" paragraph
+easySharing.addEventListener("click", function () {
+  console.log(easySharing);
+  // simpleBookmarking display: none;
+  // speedySearching display: none;
 });
