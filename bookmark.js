eace2045f08883992949b6c9559006f59b730a56
Leighton Buchanan-Cates <l.k.buchanancates@gmail.com>
2023-04-11 11:03:28 -0400

    learns about specificity and replaces id names with classes

--- bookmark.js
+++ bookmark.js
@@ -15,15 +15,15 @@ closeMobileMenuButton.addEventListener("click", function () {
 
 // FEATURES WITH BUTTONS (3 TABS) needs fixing
 const simpleBookmarkingButton = document.getElementById("simple-bookmarking");
-const simpleBookmarkingContainer = document.getElementById(
-  "simple-bookmarking-container"
+const simpleBookmarkingContainer = document.querySelector(
+  ".simple-bookmarking-container"
 );
 const speedySearchingButton = document.getElementById("speedy-searching");
-const speedySearchingContainer = document.getElementById(
-  "speedy-searching-container"
+const speedySearchingContainer = document.querySelector(
+  ".speedy-searching-container"
 );
 const easySharingButton = document.getElementById("easy-sharing");
-const easySharingContainer = document.getElementById("easy-sharing-container");
+const easySharingContainer = document.querySelector(".easy-sharing-container");
 
 // displays "bookmark in one click" paragraph WORKS but when refresh, all 3 display
 simpleBookmarkingButton.addEventListener("click", function () {
@@ -49,26 +49,33 @@ easySharingButton.addEventListener("click", function () {
 // FAQ SECTION TOGGLES
 const faqQuestion1 = document.querySelector(".faq-question-1");
 const faqAnswer1 = document.querySelector(".faq-answer-1");
+const openArrow = document.getElementById("arrow");
+
 faqQuestion1.addEventListener("click", function () {
   faqAnswer1.classList.toggle("collapse-faq-answer");
+  openArrow.classList.toggle("open-arrow");
+  // works here
 });
 
 const faqQuestion2 = document.querySelector(".faq-question-2");
 const faqAnswer2 = document.querySelector(".faq-answer-2");
 faqQuestion2.addEventListener("click", function () {
   faqAnswer2.classList.toggle("collapse-faq-answer");
+  // openArrow.classList.toggle("open-arrow");
 });
 
 const faqQuestion3 = document.querySelector(".faq-question-3");
 const faqAnswer3 = document.querySelector(".faq-answer-3");
 faqQuestion3.addEventListener("click", function () {
   faqAnswer3.classList.toggle("collapse-faq-answer");
+  // openArrow.classList.toggle("open-arrow");
 });
 
 const faqQuestion4 = document.querySelector(".faq-question-4");
 const faqAnswer4 = document.querySelector(".faq-answer-4");
 faqQuestion4.addEventListener("click", function () {
   faqAnswer4.classList.toggle("collapse-faq-answer");
+  // openArrow.classList.toggle("open-arrow");
 });
 
 // EMAIL VERIFICATION
@@ -87,7 +94,7 @@ function validation() {
   } else {
     email.classList.remove("valid");
     email.classList.add("invalid");
-    emailErrorMessage.style.visibility = "visible";
+    emailErrorMessage.textContent = "Whoops, make sure it's an email";
   }
 }
 // show error border if email entered incorrectly: style in CSS
